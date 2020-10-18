9a8623d1641b1b50a38187ec3cd15f7c4ffd67c9
Roman Ellderly <ellderly@gmail.com>
2020-10-18 23:24:24 +0300

    Готовый сайт

--- First site/js/main.js
+++ First site/js/main.js
@@ -2,16 +2,22 @@ $(function(){
     $('.button-slider-1').on('focus', function () {
         $('.slider').hide();
         $('.slider-1').addClass('animation');
-        $('.slider-1').css('display','grid');
+        $('.slider-1').css('display', 'grid');
+    
     });
     $('.button-slider-2').on('focus', function () {
         $('.slider').hide();
         $('.slider-2').addClass('animation');
-        $('.slider-2').css('display','grid');
+        $('.slider-2').css('display', 'grid');
     });
     $('.button-slider-3').on('focus', function () {
         $('.slider').hide();
         $('.slider-3').addClass('animation');
-        $('.slider-3').css('display','grid');
+        $('.slider-3').css('display', 'grid');
     });
+
+    $('.header-burger').on('click', function () {
+        $('.header-burger,nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
 });

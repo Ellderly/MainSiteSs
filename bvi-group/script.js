5559b650d1d537b9d10fec05a9ac17b46c287346
Roman Ellderly <ellderly@gmail.com>
2021-09-22 18:40:39 +0300

    adaptive menu

--- bvi-group/script.js
+++ bvi-group/script.js
@@ -8,3 +8,19 @@ new Swiper('.swiper-container', {
         delay: 3000
     },
 });
+
+const services = document.querySelector('.sectionHeader');
+const header = document.querySelector('.header');
+const navMenu = document.querySelector('.navbar')
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY;
+    let servicesCenter = services.offsetHeight + header.offsetHeight;
+    console.log(servicesCenter);
+    if(scrollTop > servicesCenter) {
+        navMenu.classList.add('fixed');
+        services.style.marginBottom = `${75}px`;
+    } else{
+        navMenu.classList.remove('fixed')
+        services.style.marginBottom = `0px`;
+    }
+})

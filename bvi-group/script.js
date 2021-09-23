6fddeacf976f4a0ff01dc212ec3f40f1ea8ca4fe
Roman Ellderly <ellderly@gmail.com>
2021-09-23 17:27:13 +0300

    adaptive

--- bvi-group/script.js
+++ bvi-group/script.js
@@ -15,7 +15,6 @@ const navMenu = document.querySelector('.navbar')
 window.addEventListener('scroll', () => {
     let scrollTop = window.scrollY;
     let servicesCenter = services.offsetHeight + header.offsetHeight;
-    console.log(servicesCenter);
     if(scrollTop > servicesCenter) {
         navMenu.classList.add('fixed');
         services.style.marginBottom = `${75}px`;
@@ -23,4 +22,11 @@ window.addEventListener('scroll', () => {
         navMenu.classList.remove('fixed')
         services.style.marginBottom = `0px`;
     }
+})
+
+const body = document.querySelector('body')
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    body.classList.toggle('block');
 })
